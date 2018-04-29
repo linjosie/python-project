8d1b10df3bf0bccceb99360b931be7e81ae572a6
linjosie <38266110+linjosie@users.noreply.github.com>
2018-04-29 15:30:04 -0400

    Update matchers.py

--- matchers.py
+++ matchers.py
@@ -3,8 +3,8 @@ import numpy as np
 
 class matchers:
 	def __init__(self):
-		self.surf = cv2.xfeatures2d.SURF_create()
-		FLANN_INDEX_KDTREE = 0
+		self.surf = cv2.xfeatures2d.SURF_create()# capture more key points than SIFT
+		FLANN_INDEX_KDTREE = 0# (Fast Library for Approximate Nearest Neighbors) to match key points
 		index_params = dict(algorithm=0, trees=5)
 		search_params = dict(checks=50)
 		self.flann = cv2.FlannBasedMatcher(index_params, search_params)
@@ -12,18 +12,15 @@ class matchers:
 	def match(self, i1, i2, direction=None):
 		imageSet1 = self.getSURFFeatures(i1)
 		imageSet2 = self.getSURFFeatures(i2)
-		print ("Direction : ", direction)
-		matches = self.flann.knnMatch(
-			imageSet2['des'],
-			imageSet1['des'],
-			k=2
-			)
+		print ("Direction : ", direction) #left of right
+		matches = self.flann.knnMatch(imageSet2['des'],imageSet1['des'],k=2) #use knn algrithm to match image
+		#apply ratio test
 		good = []
 		for i , (m, n) in enumerate(matches):
 			if m.distance < 0.7*n.distance:
 				good.append((m.trainIdx, m.queryIdx))
 
-		if len(good) > 4:
+		if len(good) > 4: #minimum matching parts
 			pointsCurrent = imageSet2['kp']
 			pointsPrevious = imageSet1['kp']
 
@@ -35,10 +32,11 @@ class matchers:
 				)
 
 			H, s = cv2.findHomography(matchedPointsCurrent, matchedPointsPrev, cv2.RANSAC, 4)
-			return H
+			return H # return transformation
 		return None
 
-	def getSURFFeatures(self, im):
-		gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+	def getSURFFeatures(self, im): # get key points and descriptor
+		gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) #turn image to gray scale
+		# find the key points and descriptors with SURF
 		kp, des = self.surf.detectAndCompute(gray, None)
 		return {'kp':kp, 'des':des}
